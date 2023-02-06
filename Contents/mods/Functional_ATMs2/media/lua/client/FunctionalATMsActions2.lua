4d72ae2386af541c1f652fa767e5c2370bfd09cc
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2023-02-06 20:56:51 +0800

    a

--- Contents/mods/Functional_ATMs2/media/lua/client/FunctionalATMsActions2.lua
+++ Contents/mods/Functional_ATMs2/media/lua/client/FunctionalATMsActions2.lua
@@ -95,42 +95,86 @@ end
 
  ]]
 
----associative array of string-patterns and values
-local itemToSellValues = {
-    ["sweater"]=0, ["hat"]=0, --["hat_goldstar"]=0,
-    ["jacket"]=0, ["paint"]=0, ["sock"]=0, ["shirt"]=0, ["pants"]=0,
+-- ---associative array of string-patterns and values
+-- local itemToSellValues = {
+    -- ["sweater"]=0, ["hat"]=0, --["hat_goldstar"]=0,
+    -- ["jacket"]=0, ["paint"]=0, ["sock"]=0, ["shirt"]=0, ["pants"]=0,
 
-    ["diamond"]=5,
+    -- ["diamond"]=5,
 
-    ["tag"]=3, ["locket"]=3,
+    -- ["tag"]=3, ["locket"]=3,
 
-    ["ruby"]=2, ["pearl"]=2, ["sapphire"]=2, ["emerald"]=2, ["amethyst"]=2, ["amber"]=2,
+    -- ["ruby"]=2, ["pearl"]=2, ["sapphire"]=2, ["emerald"]=2, ["amethyst"]=2, ["amber"]=2,
 
-    ["gold"]=1, ["silver"]=1, ["antique"]=1,
-}
+    -- ["gold"]=1, ["silver"]=1, ["antique"]=1,
+-- }
 
----creates a second table of just names and sorts them by the associative array's value
----this table is just the string-patterns to use with pairs() for consistent order
-local itemsNamesSorted = {}
-for key, value in pairs(itemToSellValues) do table.insert(itemsNamesSorted, key) end
-table.sort(itemsNamesSorted, function(a, b) return (itemToSellValues[a] < itemToSellValues[b]) end)
+-- ---creates a second table of just names and sorts them by the associative array's value
+-- ---this table is just the string-patterns to use with pairs() for consistent order
+-- local itemsNamesSorted = {}
+-- for key, value in pairs(itemToSellValues) do table.insert(itemsNamesSorted, key) end
+-- table.sort(itemsNamesSorted, function(a, b) return (itemToSellValues[a] < itemToSellValues[b]) end)
+
+-- function FunctionalATMs2.checkQty(toSell)
+
+    -- local itemName = string.lower(toSell:getName())
+
+    -- ---use sorted table with pairs() for consistent order
+    -- --- `name` (value in table) is used as a "key" in the associative array to get matching value there
+    -- for _,name in pairs(itemsNamesSorted) do
+        -- if itemName:contains(name) then
+            -- return itemToSellValues[name]
+        -- end
+    -- end
+
+    -- return 0
+-- end
 
 function FunctionalATMs2.checkQty(toSell)
-
-    local itemName = string.lower(toSell:getName())
-
-    ---use sorted table with pairs() for consistent order
-    --- `name` (value in table) is used as a "key" in the associative array to get matching value there
-    for _,name in pairs(itemsNamesSorted) do
-        if itemName:contains(name) then
-            return itemToSellValues[name]
-        end
+    if toSell:getName():contains("Sweater") or toSell:getName():contains("sweater") then
+        return 0
+    elseif toSell:getName():contains("Hat_GoldStar") then
+        return 0
+    elseif toSell:getName():contains("Hat") or toSell:getName():contains("hat") then
+        return 0
+    elseif toSell:getName():contains("Jacket") or toSell:getName():contains("jacket") then
+        return 0
+    elseif toSell:getName():contains("Paint") or toSell:getName():contains("paint") then
+        return 0
+    elseif toSell:getName():contains("Sock") or toSell:getName():contains("sock") then
+        return 0
+    elseif toSell:getName():contains("Shirt") or toSell:getName():contains("shirt") then
+        return 0
+    elseif toSell:getName():contains("Pants") or toSell:getName():contains("pants") then
+        return 0
+    elseif toSell:getName():contains("Diamond") or toSell:getName():contains("diamond") then
+        return 5
+    elseif toSell:getName():contains("Tags") or toSell:getName():contains("Tag") then
+        return 3
+    elseif toSell:getName():contains("Locket") or toSell:getName():contains("locket") then
+        return 3
+    elseif toSell:getName():contains("Ruby") or toSell:getName():contains("ruby") then
+        return 2
+    elseif toSell:getName():contains("Pearl") or toSell:getName():contains("pearl") then
+        return 2
+    elseif toSell:getName():contains("Sapphire") or toSell:getName():contains("sapphire") then
+        return 2
+    elseif toSell:getName():contains("Emerald") or toSell:getName():contains("emerald") then
+        return 2
+    elseif toSell:getName():contains("Amethyst") or toSell:getName():contains("amethyst") then
+        return 2
+    elseif toSell:getName():contains("Amber") or toSell:getName():contains("amber") then
+        return 2
+    elseif toSell:getName():contains("Gold") or toSell:getName():contains("gold") then
+        return 1
+    elseif toSell:getName():contains("Silver") or toSell:getName():contains("silver") then
+        return 1
+    elseif toSell:getName():contains("Antique") or toSell:getName():contains("antique") then
+        return 1
+    else
+        return 0
     end
-
-    return 0
 end
-
-
 function FunctionalATMs2.check(toSell)
     if sellables[toSell] then
         return true
@@ -139,19 +183,53 @@ function FunctionalATMs2.check(toSell)
     end
 end
 ------------------------               ---------------------------
+-- function FunctionalATMs2.getTotalItemsValue(atm)
+    -- local totalSellAmt = 0
+
+    -- print('-------------------------')
+    -- for i = atm:getItems():size(), 1, -1 do
+        -- local toSell = atm:getItems():get(i - 1);
+
+        -- local piecePrice = FunctionalATMs2.checkQty(toSell)
+        -- if toSell and piecePrice>0 and not (toSell:isCustomName() and toSell:isCustomName() ~= toSell:getName()) then
+            -- print('Sold ' .. toSell:getName() .. ' for ' .. piecePrice)
+            -- getPlayer():setHaloNote(tostring('Sold ' .. toSell:getName() .. ' for ' .. piecePrice))
+            -- totalSellAmt = totalSellAmt + piecePrice
+            -- atm:DoRemoveItem(toSell);
+            -- getPlayerLoot(0):refreshBackpacks()
+        -- else
+            -- getPlayerLoot(0):refreshBackpacks()
+        -- end
+    -- end
+
+    -- -- ISInventoryPage:transferAll()
+    -- -- container:clear()
+    -- return totalSellAmt
+-- end
+function FunctionalATMs2.isSellable(toSell)
+    if FunctionalATMs2.checkQty(toSell) > 0 then
+        return true
+    else
+        return false
+    end
+end
+
+
 function FunctionalATMs2.getTotalItemsValue(atm)
     local totalSellAmt = 0
-
+    -- local container = nil
+    -- local atm = containers:get(2)
+    
     print('-------------------------')
     for i = atm:getItems():size(), 1, -1 do
         local toSell = atm:getItems():get(i - 1);
-
-        local piecePrice = FunctionalATMs2.checkQty(toSell)
-        if toSell and piecePrice>0 and not (toSell:isCustomName() and toSell:isCustomName() ~= toSell:getName()) then
+        if toSell and FunctionalATMs2.isSellable(toSell) == true and
+            not (toSell:isCustomName() and toSell:isCustomName() ~= toSell:getName()) then
+            local piecePrice = FunctionalATMs2.checkQty(toSell)
             print('Sold ' .. toSell:getName() .. ' for ' .. piecePrice)
-            getPlayer():setHaloNote(tostring('Sold ' .. toSell:getName() .. ' for ' .. piecePrice))
+            getPlayer():setHaloNote(tostring('Sold ' .. toSell:getName() .. ' for ' .. piecePrice)) 
             totalSellAmt = totalSellAmt + piecePrice
-            atm:DoRemoveItem(toSell);
+            toSell:getContainer():DoRemoveItem(toSell);
             getPlayerLoot(0):refreshBackpacks()
         else
             getPlayerLoot(0):refreshBackpacks()
@@ -162,7 +240,6 @@ function FunctionalATMs2.getTotalItemsValue(atm)
     -- container:clear()
     return totalSellAmt
 end
-
 ------------------------               ---------------------------
 local function playATMsfx(player)
     getSoundManager():PlayWorldSound('ATMCash', player:getSquare(), 0, 5, 5, false);
@@ -195,7 +272,8 @@ function FunctionalATMs2.ContextMenu(char, context, worldobjects, test)
                 if spr == atmTiles[1] or spr == atmTiles[2] or spr == atmTiles[3] or spr == atmTiles[4] then
                     if not menuCreated then
                         if not instanceof(obj, "IsoThumpable") and not obj:getModData()['atmConverted']  and
-                                not obj:getContainer() and player:isAccessLevel('admin') then context:addOption("Admin: Activate ATM", spr, (function()
+                                not obj:getContainer() and player:isAccessLevel('admin') then 
+								context:addOption("Admin: Activate ATM", spr, (function()
                                 sledgeDestroy(obj)
                                 obj:getSquare():transmitRemoveItemFromSquare(obj)
                                 --[[
@@ -288,32 +366,3 @@ function FunctionalATMs2.ContextMenu(char, context, worldobjects, test)
 end
 
 Events.OnFillWorldObjectContextMenu.Add(FunctionalATMs2.ContextMenu)
--- Events.OnContainerUpdate.Add(FunctionalATMs2.checkIfGold)
-
-------------------------               
---[[ function GlytchMenu(key)
-if (key==199) then --home
-print('glytch3r')
-if not (getCore():getDebug() or isAdmin()) then return; end 
-BrushToolManager.openPanel(getPlayer())
-	return key
-	end
-end
-
-Events.OnKeyPressed.Add(GlytchMenu); ]]
---[[ 
-local toPrint = ""
-local sellables = {}
-local items = getScriptManager():getAllItems()
-for i = 0, items:size()-1  do
-        local toSell = items:get(i)
-  	
-  if  isSellable(toSell) then 
-		--print(goldItem:getName())
-	    print(toSell:getName())	
-		toPrint = toPrint..' '.. toSell:getName()  .."\n"
-	end 
-end
-Clipboard.setClipboard(toPrint)
-
- ]]
